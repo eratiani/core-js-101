2be69a18ad7cca2fb4844c2ffcb4ea2cbb0937f5
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-01-15 14:03:55 +0400

    feat: partial and idgen

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -176,8 +176,11 @@ function logger( func, logFunc ) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments( fn, ...args1 ) {
+  return (...args) => {
+   result = fn(...args1, ...args)
+   return result;
+  }
 }
 
 
@@ -198,8 +201,11 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction( startFrom ) {
+  let result = startFrom;
+  return () => {
+   return result += 1;
+  }
 }
 
 
